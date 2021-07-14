0e712b9e7cd5e2bd8bd6e9e7580d91e101b18e06
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-07-14 17:59:08 +0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public void CheckGameEnd(Transform pin)
+    {
+        if (pin.GetComponentInChildren<Text>().text == (GlobalVariables.AllPins.Count).ToString())
+        {
+            Debug.Log("GameEnded");
+        }
+    }
 }
