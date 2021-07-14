7e46d000462153596ae4ed403444bd87af8162d8
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-07-14 14:47:38 +0300

    Update PlayerBehaviour.cs

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,35 +5,31 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    [SerializeField] private bool IsGameRunning = true;
-    [SerializeField] private bool _canPlayerMove = false;
-    [SerializeField] private bool _breakMovement = false;
     [SerializeField] [Range(.001f, 10f)] private float PinSpeed = 3f;
-
     [SerializeField] private Transform TargetCircle;
-
-    public bool CanPlayerMove { get => _canPlayerMove; set => _canPlayerMove = value; }
-    public bool BreakMovement { get => _breakMovement; set => _breakMovement = value; }
 
     public void ThrowPin()
     {
-        StartCoroutine(PinThrowing(TargetCircle, GlobalVariables.CurrentPin, PinSpeed));
+        StartCoroutine(PinThrowing(TargetCircle, GlobalVariables.GetNextPin(), PinSpeed));
     }
 
     IEnumerator PinThrowing(Transform target, Transform pin, float speed)
     {
-        _canPlayerMove = false;
         float lerpVal = 0;
         Vector3 targetPos = target.position;
         Vector3 pinPos = pin.position;
-        while (lerpVal < 1 && !BreakMovement)
+        while (lerpVal < 1)
         {
             Vector3 lerpPos = Vector3.Lerp(pinPos, targetPos, lerpVal);
             pin.position = lerpPos;
             yield return null;
             lerpVal += Time.deltaTime * speed;
         }
-        _canPlayerMove = true;
     }
-    
+
+    public void BreakMovement(Transform pin)
+    {
+        StopCoroutine(PinThrowing(TargetCircle, pin, PinSpeed));
+    }
+
 }
